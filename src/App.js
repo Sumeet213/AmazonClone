4b0f61004c10987b5646e8e7b9ccc08da0ca6ed3
Sumeet213 <75944652+Sumeet213@users.noreply.github.com>
2022-10-01 13:57:48 +0530

    Authentication

--- src/App.js
+++ src/App.js
@@ -2,16 +2,41 @@ import Header from './Header';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Checkout from './Checkout';
+import Login from './Login';
+import { useEffect } from 'react';
+import { auth } from './firebase';
+import { useStateValue } from './StateProvider';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import Payment from './Payment';
 function App() {
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      console.log('The user is >>>', user);
+      if (user) {
+        const uid = user.uid;
+        dispatch({
+          type: 'SET_USER',
+          user,
+        });
+      } else {
+        dispatch({
+          type: 'SET_USER',
+          user: null,
+        });
+      }
+    });
+  }, []);
   return (
     <Router>
       <div className="App">
-        <Header />
         <Routes>
           <Route
             path="/"
             element={
               <>
+                <Header />
                 <Home />
               </>
             }
@@ -20,7 +45,26 @@ function App() {
             path="/checkout"
             element={
               <>
+                <Header />
                 <Checkout />
+              </>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <>
+                <Login />
+              </>
+            }
+          />
+
+          <Route
+            path="/payment"
+            element={
+              <>
+                <Header />
+                <Payment />
               </>
             }
           />
--- src/Subtotal.js
+++ src/Subtotal.js
@@ -2,10 +2,11 @@ import React from 'react';
 import './Subtotal.css';
 import CurrencyFormat from 'react-currency-format';
 import { useStateValue } from './StateProvider';
-
+import { getBasketTotal } from './reducer';
+import { Navigate, useNavigate } from 'react-router-dom';
 function Subtotal() {
   const [{ basket }, dispatch] = useStateValue();
-
+  const navigate = useNavigate();
   return (
     <div className="subtotal">
       <CurrencyFormat
@@ -13,7 +14,7 @@ function Subtotal() {
           <>
             <p>
               Subtotal ({basket?.length} items):
-              <strong> 0</strong>
+              <strong> {value}</strong>
             </p>
             <small className="subtotal__gift">
               <input type="checkbox" />
@@ -22,13 +23,13 @@ function Subtotal() {
           </>
         )}
         decimalScale={2}
-        value={0}
+        value={getBasketTotal(basket)}
         displayType={'text'}
         thousandSeparator={true}
         prefix={'$'}
       />
 
-      <button>Proceed to checkout</button>
+      <button onClick={(e) => navigate('/payment')}>Proceed to checkout</button>
     </div>
   );
 }
